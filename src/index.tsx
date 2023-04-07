071ba4348674ee0bb50b2669ea9532ea42db4d62
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2023-04-07 03:55:56 +0100

    add redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import CodeCell from './components/code-cell';
 import TextEditor from './components/text-editor';
+import { store } from './state';
 
 const App = () => {
   return (
-    <div>
-      {/* <CodeCell /> */}
-      <TextEditor/>
-    </div>
+  <Provider store={store}>
+
+      <div>
+
+        <TextEditor />
+      </div>
+  </Provider>
   );
 };
 
