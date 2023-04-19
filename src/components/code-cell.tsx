267b06c4a3c35f9c23a4555bf3b659ba87417bf1
erdaldincer <erdldncr@gmail.com>
2023-04-19 02:53:35 +0100

    added bundle show function

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -14,20 +14,45 @@ const CodeCell: React.FC<CodeCellProps> = ({cell}) => {
 const {updateCell, createBundle} = useActions()
 const bundle =usedTypedSelector((state)=>state.bundles[cell.id])
 
+
+const cumulativeCode=usedTypedSelector((state)=>{
+  const {data, order}= state.cells;
+  const orderedCells= order.map((id)=>data[id])
+  const cumulativeCode=[
+    `const show=(value)=>{
+      if(typeof value === 'object'){
+        document.querySelector("#root").innerHTML= JSON.stringtify(value);
+      }else{
+        document.querySelector("#root").innerHTML= value;
+      }
+      
+     }`
+  ];
+  for(let c of orderedCells){
+    if(c.type==="code"){
+      cumulativeCode.push(c.content)
+    }
+        if(c.id===cell.id){
+ break;
+    }
+  }
+  return cumulativeCode;
+})
+
   useEffect(() => {
     if(!bundle){
-      createBundle(cell.id,cell.content)
+      createBundle(cell.id,cumulativeCode.join('\n'))
       return
     }
     const timer = setTimeout(async () => {
-createBundle(cell.id,cell.content)
+createBundle(cell.id,cumulativeCode.join('\n'))
      
     }, 750);
 
     return () => {
       clearTimeout(timer);
     };
-  }, [cell.content,cell.id, createBundle]);
+  }, [cumulativeCode.join('\n'),cell.id, createBundle]);
 
   return (
     <Resizable direction="vertical">
